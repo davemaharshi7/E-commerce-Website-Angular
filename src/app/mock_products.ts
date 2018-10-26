35f69181d7a70ab38c75fe536659e87ded5e8be6
davemaharshi7 <39943075+davemaharshi7@users.noreply.github.com>
2018-10-27 01:32:49 +0530

    one more
    one more commit

--- src/app/mock_products.ts
+++ src/app/mock_products.ts
@@ -5,9 +5,9 @@ export const PRODS:Products[]=[
         id: 1,
         name:"Titan Watch Model 1",
         price:1800,
-        description:"NULL",
+        description:"Titan Company Limited (earlier known as Titan Industries Limited) is an Indian luxury goods company. It is a joint venture between the TATA Group.",
         stock:5,
-        imageurl:["assets/img/products/watch1.jpeg"],
+        imageurl:["assets/img/products/watch1.jpeg","assets/img/products/watch_1_1.jpeg","assets/img/products/watch_1_2.jpeg"],
         category:"WATCH"
     },
 
@@ -15,9 +15,9 @@ export const PRODS:Products[]=[
         id: 2,
         name:"Titan Watch Model 2",
         price:2000,
-        description:"NULL",
+        description:"Titan Company Limited (earlier known as Titan Industries Limited) is an Indian luxury goods company. It is a joint venture between the TATA Group.",
         stock:5,
-        imageurl:["assets/img/products/watch2.jpeg"],
+        imageurl:["assets/img/products/watch2.jpeg","assets/img/products/titan_2_1.jpeg","assets/img/products/titan_2_2.jpeg"],
         category:"WATCH"
     },
 
@@ -26,9 +26,9 @@ export const PRODS:Products[]=[
         id: 3,
         name:"Galaxy Note 9",
         price:67900,
-        description:"NULL",
+        description:"Samsung Galaxy Note 8 is an Android Smartphone Designed, Developed and Marketed by Samsung Electronics",
         stock:0,
-        imageurl:["assets/img/products/note9_1.jpeg"],
+        imageurl:["assets/img/products/note9_1.jpeg","assets/img/products/note9_2.jpeg","assets/img/products/note9_4.jpeg"],
         category:"ELECTRONICS"
     },
 
@@ -37,9 +37,9 @@ export const PRODS:Products[]=[
         id: 4,
         name:"Poco F1",
         price:23999,
-        description:"NULL",
+        description:" Meet the POCO F1 - the first flagship smartphone from POCO by Xiaomi. The POCO F1 sports Qualcomm flagship Snapdragon 845 processor, an octa-core CPU with a maximum clock speed of 2.8 GHz which is supported by 8 GB of LPDDR4X RAM. ",
         stock:0,
-        imageurl:["assets/img/products/pocof1.jpeg"],
+        imageurl:["assets/img/products/pocof1.jpeg","assets/img/products/poco_1.jpeg","assets/img/products/poco_2.jpeg"],
         category:"ELECTRONICS"
     },
 
@@ -47,9 +47,9 @@ export const PRODS:Products[]=[
         id: 5,
         name:"Wrangler Men's Shirt",
         price:1500,
-        description:"NULL",
+        description:"Tony Y/D check Shirt",
         stock:5,
-        imageurl:["assets/img/products/mshirt1.jpeg"],
+        imageurl:["assets/img/products/mshirt1.jpeg","assets/img/products/mshirt1_1.jpeg","assets/img/products/mshirt2_2.jpeg"],
         category:"CLOTHING"
     },
 
@@ -58,9 +58,9 @@ export const PRODS:Products[]=[
         id: 6,
         name:"Maniac Checkered Men's Round Neck Tshirt",
         price:450,
-        description:"NULL",
+        description:"Its a full sleeve Mens Shirt.",
         stock:5,
-        imageurl:["assets/img/products/mshirt2.jpeg"],
+        imageurl:["assets/img/products/mshirt2.jpeg","assets/img/products/mshirt2-1.jpeg","assets/img/products/mshirt2-2.jpeg"],
         category:"CLOTHING"
     },
 
@@ -68,9 +68,9 @@ export const PRODS:Products[]=[
         id: 7,
         name:"Leather Sofa 1 Seater",
         price:26550,
-        description:"NULL",
+        description:"One of the most comfortable Sofa in affordable price. ",
         stock:5,
-        imageurl:["assets/img/products/sofa1.jpeg"],
+        imageurl:["assets/img/products/sofa1.jpeg","assets/img/products/sofa2.jpeg","assets/img/products/sofa1.jpeg"],
         category:"FURNITURE"
     },
 
@@ -78,9 +78,9 @@ export const PRODS:Products[]=[
         id: 8,
         name:"Shrey Cricket Helmet",
         price:1550,
-        description:"NULL",
+        description:"Its very Lightweight and strong helmet.",
         stock:5,
-        imageurl:["assets/img/products/cricket1.jpeg"],
+        imageurl:["assets/img/products/cricket1.jpeg","assets/img/products/cricket2.jpeg","assets/img/products/cricket3.jpeg"],
         category:"SPORTS"
     },
 
@@ -98,9 +98,9 @@ export const PRODS:Products[]=[
         id: 10,
         name:"4K UHD TV ",
         price:155500,
-        description:"SONY BRAVIA 55 inches Black color",
+        description:"With a perfect combination of stunning 4K picture quality and impressive sound quality, this 139-cm (55) Ultra HD 4K LED Smart TV from Sony is an ideal choice for anyone who loves to watch movies or play games.",
         stock:5,
-        imageurl:["assets/img/tvproduct.jpg"],
+        imageurl:["assets/img/products/tv1.jpeg","assets/img/products/tv2.jpeg","assets/img/products/tv3.jpeg"],
         category:"ELECTRONICS"
     },
 
@@ -110,7 +110,7 @@ export const PRODS:Products[]=[
         price:25000,
         description:"42 mm Space Grey Aluminium Case with Grey Sport Band (Grey Strap Regular)",
         stock:5,
-        imageurl:["assets/img/apple2.jpg"],
+        imageurl:["assets/img/apple2.jpg","assets/img/products/apple2.jpeg","assets/img/products/apple1.jpeg"],
         category:"ELECTRONICS"
     },
     
@@ -120,7 +120,7 @@ export const PRODS:Products[]=[
         price:65000,
         description:"Its integrated with most powerfull Cpu and Hardware.",
         stock:5,
-        imageurl:["assets/img/laptop-1.jpg"],
+        imageurl:["assets/img/laptop-1.jpg","assets/img/products/laptop2.jpeg","assets/img/products/laptop1.jpeg"],
         category:"ELECTRONICS"
     },
 ]
